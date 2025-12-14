d63a65ca3132b56c1c26ceb6cae852d490290cb6
armin <a.s-dev@web.de>
2025-12-14 12:14:47 +0100

    remove all and init deno

--- controllers/error.js
+++
@@ -1,9 +0,0 @@
-const send404Page = (req, res, next) => {
-  res.status(404).render("404", { pageTitle: "Page not Found", path: "/" });
-};
-
-const send500Page = (req, res, next) => {
-  res.status(500).render("500", { pageTitle: "Error", path: "/500" });
-};
-
-module.exports = { send404Page, send500Page };
